62b61282d36a51ca746842ef36b3b548a258a6fe
Aly Rasmy <alyrasmy94@gmail.com>
2021-05-06 10:19:23 +0200

    Pushing solution code for dev labs available on courselabs.solace.com and those solution files are linked from there to take participants to this github repo

---
+++ devlabs-solution-code/jms/connect/TopicSubscriber.java
@@ -0,0 +1,89 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ *  Solace JMS 1.1 Examples: TopicSubscriber
+ */
+
+import java.util.concurrent.CountDownLatch;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import com.solacesystems.jms.SolConnectionFactory;
+import com.solacesystems.jms.SolJmsUtility;
+
+/**
+ * Subscribes to messages published to a topic using Solace JMS 1.1 API implementation.
+ *
+ * This is the Subscriber in the Publish/Subscribe messaging pattern.
+ */
+public class TopicSubscriber {
+
+    public void run(String... args) throws Exception {
+        String[] split = args[1].split("@");
+
+        String host = args[0];
+        String vpnName = split[1];
+        String username = split[0];
+        String password = args[2];
+
+        System.out.printf("TopicSubscriber is connecting to Solace messaging at %s...%n", host);
+
+        // Programmatically create the connection factory using default settings
+        SolConnectionFactory connectionFactory = SolJmsUtility.createConnectionFactory();
+        connectionFactory.setHost(host);
+        connectionFactory.setVPN(vpnName);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        Connection connection = connectionFactory.createConnection();
+
+        // Create a non-transacted, Auto ACK session.
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        System.out.printf("Connected to Solace Message VPN '%s' with client username '%s'.%n", vpnName,
+                username);
+
+        session.close();
+        connection.close();
+    }
+
+    public static void main(String... args) throws Exception {
+        if (args.length != 3 || args[1].split("@").length != 2) {
+            System.out.println("Usage: TopicSubscriber <host:port> <client-username@message-vpn> <client-password>");
+            System.out.println();
+            System.exit(-1);
+        }
+        if (args[1].split("@")[0].isEmpty()) {
+            System.out.println("No client-username entered");
+            System.out.println();
+            System.exit(-1);
+        }
+        if (args[1].split("@")[1].isEmpty()) {
+            System.out.println("No message-vpn entered");
+            System.out.println();
+            System.exit(-1);
+        }
+        new TopicSubscriber().run(args);
+    }
+}
